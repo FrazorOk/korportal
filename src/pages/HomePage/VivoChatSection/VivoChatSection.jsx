3895e83205b5eafed889fb8bec381da2064b561c
FrazorOk <Dimoniw1996@gmail.com>
2024-10-09 09:34:08 +0300

    from offic

--- src/pages/HomePage/VivoChatSection/VivoChatSection.jsx
+++ src/pages/HomePage/VivoChatSection/VivoChatSection.jsx
@@ -4,11 +4,7 @@ const VivoChatSection = () => {
 	return (
 		<div className={s.chat}>
 			<h3>Наші новини</h3>
-			<iframe
-				name="embed-feed"
-				title="Viva Engage"
-				src="https://engage.cloud.microsoft/embed/groups/eyJfdHlwZSI6Ikdyb3VwIiwiaWQiOiIxODMzNjExMjY0MDAifQ?header=true&footer=true&hideNetworkName=false&theme=light&includeFeedInformation=true"></iframe>
-			{/* <iframe src="https://engage.cloud.microsoft/embed/groups/eyJfdHlwZSI6Ikdyb3VwIiwiaWQiOiIxODMzNjExMjY0MDAifQ?header=false&footer=false&theme=light&includeFeedInformation=false"></iframe> */}
+			<iframe src="https://engage.cloud.microsoft/embed/groups/eyJfdHlwZSI6Ikdyb3VwIiwiaWQiOiIxODMzNjExMjY0MDAifQ?header=false&footer=false&theme=light&includeFeedInformation=false"></iframe>
 		</div>
 	);
 };
