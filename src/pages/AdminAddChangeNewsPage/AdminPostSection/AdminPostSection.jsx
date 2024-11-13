2ceb6ea2e1b2c600ea9fb9341af7dfa8e3d78aab
FrazorOk <Dimoniw1996@gmail.com>
2024-11-13 17:49:34 +0200

    office end

--- src/pages/AdminAddChangeNewsPage/AdminPostSection/AdminPostSection.jsx
+++ src/pages/AdminAddChangeNewsPage/AdminPostSection/AdminPostSection.jsx
@@ -2,18 +2,62 @@ import s from './AdminPostSection.module.css';
 import smileIcon from '../../../assets/img/icons/smile-icon.svg';
 import deleteIcon from '../../../assets/img/icons/delete-icon.svg';
 import EmojiList from '../../../components/EmojiList/EmojiList';
-import { useEffect, useState } from 'react';
+import { createNewPost } from '../../../api/api.js';
+import { useEffect, useState, useRef } from 'react';
 
 const AdminPostSection = ({ newsId, data }) => {
 	let [visibleStatus, setVisibleStatus] = useState(false);
 	let [visibleStatus2, setVisibleStatus2] = useState(false);
 	let [title, setTitle] = useState('');
+	let [date, setDate] = useState('дд.мм.рррр 00:00:00');
 	let [text, setText] = useState('');
+	let [tags, setTags] = useState('');
+	let [images, setImages] = useState('');
+	let [comment, setComment] = useState([]);
+	let ref = useRef();
+
+	const selectHandler = (e) => {
+		e.preventDefault();
+
+		// let data = {
+		// 	title: title,
+		// 	date: date,
+		// 	tags: tags,
+		// 	text: text,
+		// 	imgUrl: '',
+		// 	imgFile: '',
+		// 	comments: [
+		// 		{
+		// 			autor: '',
+		// 			comment_txt: '',
+		// 			post_date: '',
+		// 		},
+		// 	],
+		// };
+
+		let data = {
+			title: title,
+			tags: tags.split(', '),
+			date: date,
+			text: text,
+			imgFile: ref.current.files[0],
+			cat_id: 1,
+		};
+
+		createNewPost(data);
+	};
 
 	useEffect(() => {
 		setTitle(data.title);
+		setDate(data.pub_date);
+		data.tags && data.tags.length > 0 && setTags(data.tags.join(', '));
 		setText(data.text);
+		setImages(data.img);
+		setComment(data.comment);
 	}, [data]);
+	useEffect(() => {
+		console.log(comment);
+	}, [comment]);
 
 	return (
 		<div className={`section-container`}>
@@ -46,17 +90,13 @@ const AdminPostSection = ({ newsId, data }) => {
 						</span>
 					</p>
 					<p className={s.date}>
-						<input type="date" />
-					</p>
-					<p>або зараз:</p>
-					<p>
-						<input className={s.now} type="checkbox" />
+						<input type="datetime-local" step="1" onChange={(e) => setDate(e.target.value)} value={date} />
 					</p>
 				</div>
 
 				<div className={s.post_item}>
 					<p>Теги:</p>
-					<input placeholder="Теги, теги, ..." type="text" />
+					<input placeholder="Теги, теги, ..." type="text" value={tags} onChange={(e) => setTags(e.target.value)} />
 				</div>
 
 				<div className={s.post_item}>
@@ -78,6 +118,7 @@ const AdminPostSection = ({ newsId, data }) => {
 					</button>
 					<EmojiList visibleStatus={visibleStatus2} setSmile={setText} />
 				</div>
+
 				<div className={s.post_item}>
 					<p>
 						Зображення:
@@ -85,25 +126,56 @@ const AdminPostSection = ({ newsId, data }) => {
 							*
 						</span>
 					</p>
-					<input className={s.image} type="file" name="myImage" />
+
+					{images && (
+						<div className={s.image_container}>
+							<img className={s.image_url} src={images} alt="" />
+							<button
+								onClick={(e) => {
+									e.preventDefault();
+									setImages('');
+								}}
+								title="Видалити зображення">
+								<img src={deleteIcon} alt="" />
+							</button>
+						</div>
+					)}
+
+					<input ref={ref} className={s.image} type="file" name="myImage" />
 				</div>
 
-				{newsId ? (
+				{comment && comment.length > 0 ? (
 					<div className={s.post_item}>
 						<p>Коментарі:</p>
 						<div className={s.comments_list}>
-							<div className={s.comment}>
-								<p>dsf sdf sdf sdf</p>
-								<button title="Видалити коментар">
-									<img src={deleteIcon} alt="" />
-								</button>
-							</div>
-							<div className={s.comment}>
-								<p>dsf sdf sdf sdf</p>
-								<button title="Видалити коментар">
-									<img src={deleteIcon} alt="" />
-								</button>
-							</div>
+							{comment.map((item) => (
+								<div className={s.comment}>
+									<div className={s.comment_column}>
+										<p style={{ color: '#004795' }}>{item.autor}</p>
+										<p style={{ color: '#7d7d7d', fontSize: '12px' }}>{item.post_date}</p>
+										<p>{item.comment_txt}</p>
+									</div>
+									<button
+										onClick={(e) => {
+											e.preventDefault();
+
+											setComment((el) => {
+												let currentArray = [];
+												el.forEach((element) => {
+													if (element != item) {
+														currentArray.push(element);
+													}
+												});
+												console.log(currentArray);
+
+												return currentArray;
+											});
+										}}
+										title="Видалити коментар">
+										<img src={deleteIcon} alt="" />
+									</button>
+								</div>
+							))}
 						</div>
 					</div>
 				) : (
@@ -112,7 +184,9 @@ const AdminPostSection = ({ newsId, data }) => {
 
 				<div className={s.select_buttons}>
 					<button className={s.cancel}>Відмінити</button>
-					<button className={s.select}>Опублікувати</button>
+					<button className={s.select} onClick={selectHandler}>
+						Опублікувати
+					</button>
 				</div>
 			</form>
 		</div>
